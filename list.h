614b28fcdbed6553d537246eb139b48434f01f4f
Paul <james.swift@yandex.ru>
2016-09-26 23:37:26 +0700

    Add files via upload

---
+++ list.h
@@ -0,0 +1,25 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <string.h>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+typedef struct listnode {
+    struct listnode *next;
+    char *key;
+    int val;
+} listnode;
+
+listnode *listnode_create(int val, char *key);
+listnode *listnode_add(listnode *root, int val, char *key);
+listnode *listnode_add_after(listnode *root, int val, char *key, int k);
+listnode *listnode_delete(listnode *root, int k);
+listnode *listnode_move(listnode *root, int p, int k);
+int listnode_count(listnode *root);
+void listnode_print(listnode *root);
+void printStack(listnode*);
+
+#endif
